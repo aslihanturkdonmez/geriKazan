3ec16357cc0d4ff58c078c870a7bdc1603039209
aslihanturkdonmez <asliturkdonmez@hotmail.com>
2022-03-22 00:52:58 +0300

    added home screen

--- src/Router.js
+++ src/Router.js
@@ -1,27 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-//import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import {Login} from './screens';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {Login, SignUp, Home} from './screens';
 
+
+/* const MyTheme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      background:'#d0e1d0',
+    },
+  }; */
 
 const Stack=createNativeStackNavigator();
-//const Tab=createBottomTabNavigator();
+const Tab=createBottomTabNavigator();
 
 const Router = () => {
+    const [auth, setAuth] = useState(false);
 
     const AuthStack = () => {
         return (
             <Stack.Navigator screenOptions={{headerShown: false}}>
                 <Stack.Screen name='Login' component={Login} />
+                <Stack.Screen name="SignUp" component={SignUp} />
             </Stack.Navigator>
         )
     }
 
+
+
     return ( 
-        <NavigationContainer>
-            <AuthStack />
+        <NavigationContainer /* theme={MyTheme} */>
+            { auth ?
+                <AuthStack />
+            :
+                <Tab.Navigator screenOptions={{headerShown: false}}>
+                    <Tab.Screen name='Home' component={Home} />
+                </Tab.Navigator>
+            }
         </NavigationContainer>
      );
 }
---
+++ src/screens/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Selam</Text>
+        </View>
+      );
+}
+ 
+export default Home;
