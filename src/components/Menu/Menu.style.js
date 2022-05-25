b0c33eeca6630787652768880dcb1daee6741b19
aslihanturkdonmez <asliturkdonmez@hotmail.com>
2022-05-25 22:05:26 +0300

    created account stack

---
+++ src/components/Menu/Menu.style.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container:{
+
+    },
+    CardWrapper:{
+        justifyContent:'space-between',
+        flexDirection:'row',
+        alignItems:'center',
+        paddingRight:15,
+    },
+});
+
+export default styles;
